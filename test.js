c53d21532cfdefddcdcab3339c79de3ba1b4cce9
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import rejectP from "./source.js"
+import reduceWithValueKey from "./source"
 
 test(({equal}) => {
   return rejectP("a")
